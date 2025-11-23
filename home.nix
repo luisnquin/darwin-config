642f77745add82ed9a15bc531fb365e45d1b8496
Luis Quiñones Requelme <luis@quinones.pro>
2025-11-22 23:16:46 -0500

    feat: fzf setup

--- home.nix
+++ home.nix
@@ -27,6 +27,40 @@
   programs = {
     fish.enable = true;
     zsh.enable = true;
+    fzf = {
+      enable = true;
+      enableFishIntegration = true;
+      enableZshIntegration = true;
+      colors = {
+        "fg" = "#e8d4f3";
+        "fg+" = "#d0d0d0";
+        "bg" = "-1";
+        "bg+" = "#221326";
+        "hl" = "#4eb0d0";
+        "hl+" = "#4eb8dc";
+
+        "spinner" = "#af5fff";
+        "marker" = "#782cce";
+        "prompt" = "#3d848b";
+        "pointer" = "#af5fff";
+        "header" = "#87afaf";
+        "info" = "#d2d286";
+
+        "border" = "#262626";
+        "query" = "#d9d9d9";
+        "label" = "#aeaeae";
+      };
+
+      defaultOptions = [
+        "--border=rounded"
+        "--preview-window=border-rounded"
+        "--prompt='> '"
+        "--marker='>'"
+        "--pointer='◆'"
+        "--separator='─'"
+        "--scrollbar='│'"
+      ];
+    };
   };
 
   programs.vscode = {
