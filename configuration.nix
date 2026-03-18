61c73a98adf1810332c997473cb7aff4f5e59375
Luis Quiñones Requelme <luis@quinones.pro>
2026-03-17 19:59:17 -0500

    feat: ssh setup

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,14 @@
     config.allowUnfree = true;
   };
 
+  users.users.luisnquin = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXW6vsDRgI/AiOdGnQOTyiz1uLFL0o66u0Ahcw9VWyd luis@quinones.pro"
+    ];
+  };
+
+  services.openssh.enable = true;
+
   programs.fish.enable = true;
   shared.aliases.enable = true;
   system.primaryUser = "luisnquin";
