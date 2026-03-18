3fd0fe3f59c7ba09c1a715b38eaa2cdb3c8f4dd8
Luis Quiñones Requelme <luis@quinones.pro>
2026-03-18 10:29:32 -0500

    feat: use nixpkgs/nixos-unstable when I am not meant to do that

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager?ref=master";
       inputs.nixpkgs.follows = "nixpkgs";
