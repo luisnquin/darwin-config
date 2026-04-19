2029b4787d681a26defd94b6ca92b091faf136cf
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-19 01:17:23 -0500

    feat: configure ssh for darwin

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,20 @@
     ];
   };
 
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    extraConfig = ''
+      PasswordAuthentication no
+      KbdInteractiveAuthentication no
+      AuthenticationMethods publickey
+      PubkeyAuthentication yes
+      PermitRootLogin no
+      MaxAuthTries 3
+      AllowUsers ${config.system.primaryUser}
+      ClientAliveCountMax 3
+      ClientAliveInterval 60
+    '';
+  };
 
   programs.fish.enable = true;
 
