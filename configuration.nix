c70f91eda75971d717977d4241cfca3b6cb32e27
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-19 01:12:21 -0500

    feat: add new authorized key

--- configuration.nix
+++ configuration.nix
@@ -44,6 +44,7 @@
   users.users.luisnquin = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXW6vsDRgI/AiOdGnQOTyiz1uLFL0o66u0Ahcw9VWyd luis@quinones.pro"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICOvNB4XZFchiWUCpdXaNcyoyUi9+7SnGCvrRk2CM129"
     ];
   };
 
