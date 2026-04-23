950459ae57c06083ba6b78bf1973c707617f8a20
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-22 20:04:38 -0500

    feat(darwin/defaults): more settings

--- modules/darwin/defaults.nix
+++ modules/darwin/defaults.nix
@@ -22,6 +22,38 @@
         KeyRepeat = 2;
         NSAutomaticWindowAnimationsEnabled = false;
         NSWindowResizeTime = 0.1;
+        NSDisableAutomaticTermination = true;
+      };
+
+      finder = {
+        NewWindowTarget = "Computer";
+        FXRemoveOldTrashItems = true;
+        FXEnableExtensionChangeWarning = false;
+      };
+
+      screencapture = {
+        target = "clipboard";
+        show-thumbnail = false;
+        disable-shadow = true;
+        type = "jpg";
+      };
+
+      screensaver = {
+        askForPassword = true;
+        askForPasswordDelay = 60 * 20;
+      };
+
+      CustomUserPreferences = {
+        "com.apple.desktopservices" = {
+          DSDontWriteUSBStores = true;
+          DSDontWriteNetworkStores = true;
+        };
+        "com.apple.screencapture" = {
+          "disable-shadow" = true;
+        };
+        "com.apple.finder" = {
+          DisableAllAnimations = true;
+        };
       };
 
       ".GlobalPreferences" = {
