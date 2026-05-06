c956f70755dfdd6b693048a980722d0daeecfb8f
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-06 09:34:45 -0500

    feat(dock): add Safari and System Settings

--- modules/darwin/dock.nix
+++ modules/darwin/dock.nix
@@ -5,6 +5,8 @@
       username = config.system.primaryUser;
       entries = [
         {path = "/System/Applications/App Store.app";}
+        {path = "/Applications/Safari.app/";}
+        {path = "/System/Applications/System Settings.app";}
         {path = "/Applications/Ghostty.app/";}
       ];
     };
