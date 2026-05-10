8792fe60c215ec6eb05217b937f973612454b622
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-10 09:21:55 -0500

    fix(ai/ollama): listen all interfaces

--- modules/home/ai/ollama.nix
+++ modules/home/ai/ollama.nix
@@ -2,6 +2,8 @@
   flake.modules.homeManager.aiOllama = {
     services.ollama = {
       enable = true;
+      host = "0.0.0.0";
+      port = 11434;
       loadModels = [
         "gemma4:e4b"
         "qwen2.5-coder:7b"
