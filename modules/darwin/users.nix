4325f06f2db335f13e0227440d3bf2143da284f4
Luis Quiñones <lpaandres2020@gmail.com>
2026-09-04 22:02:17 -0500

    feat: refresh pubkey

--- modules/darwin/users.nix
+++ modules/darwin/users.nix
@@ -4,8 +4,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXW6vsDRgI/AiOdGnQOTyiz1uLFL0o66u0Ahcw9VWyd luis@quinones.pro"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICOvNB4XZFchiWUCpdXaNcyoyUi9+7SnGCvrRk2CM129"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM3ELYeyuf9nzd+M6Y2ksJUl0B7L24iVFIOLCCS8Rb7L hotline-emulator"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPidNFv02W5+F9mlSjIkMLKBef9qVxK+yHoOI/qQ23i hotline"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOy0naPj/yYMQtfKt/geDwWyS16IujRV3UbC4P2xQalE"
       ];
     };
   };
